2de894ab2668dc684cef060d5ae3256f34220232
MD1809 <dung2k5k58lx@gmail.com>
2024-08-29 19:32:52 +0700

    bài tập week 1

--- OOP_NO3_Week.1/PassObject.java
+++ OOP_NO3_Week.1/PassObject.java
@@ -4,7 +4,7 @@ public class PassObject {
     }
     public static void main(String[] args) {
         Number n = new Number();//Tạo một đối tượng n từ lớp Number 
-        n.i = 14;//gán giá trị 14 cho thuộc tính i
+        n.i = 14;//gán giá trị 14 cho thuộc tính i.
         f(n); // what is n.i now? n.i = 15(Khi n được truyền vào phương thức f, đối tượng m trong phương thức f tham chiếu đến cùng một đối tượng mà n đang tham chiếu. Do đó, khi m.i được gán giá trị 15 bên trong phương thức f, thuộc tính i của đối tượng n cũng sẽ thay đổi thành 15).
     }
 }
