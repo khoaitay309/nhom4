4ca8047f5e34611afea9c23c996b11bb12289b21
MD1809 <dung2k5k58lx@gmail.com>
2024-08-29 12:20:29 +0000

    bài tập Week 1

---
+++ OOP_NO3_Week.1/if_else.java
@@ -0,0 +1,9 @@
+public class if_else {
+    public static void main(String[] args){
+        int x = 50;
+        int y = 10;
+        if(x > y){
+            System.out.println("Hello World");
+        }
+    }
+}
