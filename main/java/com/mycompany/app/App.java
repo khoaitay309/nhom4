b3839f199ed3c2594145e1c902fb9b7c2588db96
MD1809 <dung2k5k58lx@gmail.com>
2024-08-30 04:42:09 +0000

    week 2

--- main/java/com/mycompany/app/App.java
+++ main/java/com/mycompany/app/App.java
@@ -1,9 +1,12 @@
 public class App{
     public static void main(String[] args){
-        Book myBook = new Book();
-        Time myTime = new Time();
-        Recursion myRecursion = new Recursion();
-        NNcollection myNNcollection = new NNcollection();
-        NameNumber myNameNumber = new NameNumber();
+        Book myBook = new Book("TheTexBook", "Donald knuth", 483);
+        //Time myTime = new Time();
+        //Recursion myRecursion = new Recursion();
+        //NNcollection myNNcollection = new NNcollection();
+        //NameNumber myNameNumber = new NameNumber();
+        System.out.println(myBook.title);
+        System.out.println(myBook.author);
+        System.out.println(myBook.numPages);
     }
 }
--- main/java/com/mycompany/app/Book.java
+++ main/java/com/mycompany/app/Book.java
@@ -1,3 +1,11 @@
 public class Book{
-
+    String title;
+    String author;
+    int numPages;
+    
+    public Book(String t, String a, int p) {
+        this.title = t;
+        this.author = a;
+        this.numPages = p;
+    }
 }
