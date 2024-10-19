601aa02c9c7748d88fc3892f5725807c710d6f31
MD1809 <dung2k5k58lx@gmail.com>
2024-10-19 18:09:52 +0700

    don branch

--- OOP_NO3_Week.1/if_else.java
+++
@@ -1,9 +0,0 @@
-public class if_else {
-    public static void main(String[] args){
-        int x = 50;
-        int y = 10;
-        if(x > y){
-            System.out.println("Hello World");
-        }
-    }
-}
